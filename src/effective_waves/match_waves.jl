5e0e03972a95d6ff2b0a79718159131c1ed1ea5f
ArtGower <arturgower@gmail.com>
2018-08-03 10:13:20 +0100

    testing matching vs extinction

--- src/effective_waves/match_waves.jl
+++ src/effective_waves/match_waves.jl
@@ -69,6 +69,47 @@ function match_arrays(ω::T, wave_effs::Vector{EffectiveWave{T}}, XL::Int, X::Ab
     return (L_mat, E_mat, (im*k^2*inv_w).*invV*conj(w_vec))
 end
 
+
+"
+Returns inv(V)*Y, which connect the effective and average wave through α = inv(V)*Y*A.
+The matching region is X[XL:end].
+"
+function match_only_arrays(ω::T, wave_effs::Vector{EffectiveWave{T}}, XL::Int, X::AbstractVector{T}, medium::Medium{T}, species::Vector{Specie{T}}; θin::T = 0.0) where T<:Number
+
+    XJ = length(X)
+    k = ω/medium.c
+    hos = union(w.hankel_order for w in wave_effs)
+    if length(hos) > 1
+        warn("Expected all effective waves to have the same hankel order!")
+    end
+    ho = minimum(hos)
+    s = length(species)
+
+    Z = OffsetArray{Complex{Float64}}(-ho:ho, 1:s);
+    for m = 0:ho, l = 1:s
+        Z[m,l] = Zn(ω,species[l],medium,m)
+        Z[-m,l] = Z[m,l]
+    end
+
+    σ = integration_scheme(X[1:XL]; scheme=:trapezoidal) # integration scheme: trapezoidal
+
+    avg_wave_effs = [AverageWave(real(k), wave, X) for wave in wave_effs]
+    vs = [
+        [w.amplitudes[j,n+ho+1,1] for w in avg_wave_effs]
+    for j = XL:XJ, n = -ho:ho]
+
+    invV = inv(sum(conj(vs)[inds] * transpose(vs[inds])  for inds in eachindex(vs)))
+
+    invVY = hcat(
+        [
+            invV * [(j < XL) ? zero(Complex{T}) : conj(w.amplitudes[j,n+ho+1,1]) for w in avg_wave_effs]
+        for j = 1:XJ, n = -ho:ho]...
+    )
+
+    return invVY
+end
+
+
 "
 returns (w_vec, q_arr), which leads to the extinction equation  sum(w_vec.*α) = im*k^2 + sum(q_arr[:].*A_avg[:]).
 The index XL indicates that X[XL] is the first point in the matching region.
--- src/effective_waves/match_waves_test.jl
+++ src/effective_waves/match_waves_test.jl
@@ -16,33 +16,35 @@ tol = 1e-7
 # ho = 3
 ho = maximum_hankel_order(ω, medium, [specie]; tol=tol*1e4)
 
+# for X = 0.0:0.05:12. the relative convergence error for
+# Specie(ρ=0.5, r=0.5, c=0.2, volfrac=0.1), k = 0.8, and θin = 0.0,
+#  is between 6 and 8 is 0.05%
 X = 0.0:0.05:12.
-# XJ = length(X)
-XJ = Int(round(length(X)/2 + length(X)/10))
-XL = Int(round(length(X)/2 - length(X)/10))
-XJ = Int(round(length(X)/2 + 20))
-XL = Int(round(length(X)/2 - 20))
-Xend =  Int(round(length(X)/2 + length(X)/4))
-X_match = X[XL:XJ]
 
 # Calculate discretised average wave directly from governing equations
 avg_wave_X = AverageWave(ω, medium, specie; hankel_order=ho, X=X)
+
+using Plots; pyplot()
+scatter(avg_wave_X.X, real.(avg_wave_X.amplitudes[:,1]))
+
+XJ = Int(round(length(X)/2 + 10))
+XL = Int(round(length(X)/2 - 10))
+Xend =  Int(round(length(X)/2 + length(X)/4))
+X_match = X[XL:XJ]
 avg_wave = AverageWave(ho, X[1:XJ], avg_wave_X.amplitudes[1:XJ,:,:])
 
 # From effective wave theory
-k_effs = wavenumbers(ω, medium, [specie];
-    mesh_size = 0.2, mesh_points = 20,
+k_effs_all = wavenumbers(ω, medium, [specie];
+    mesh_size = 0.4, mesh_points = 10,
     tol = tol/10, hankel_order = ho
 )
 
-using Plots; pyplot()
-
-scatter(real.(k_effs),imag.(k_effs), title = "Effective wavenumbers", xlab = "Re k_eff", ylab = "Im k_eff")
+scatter(real.(k_effs_all),imag.(k_effs_all), title = "Effective wavenumbers", xlab = "Re k_eff", ylab = "Im k_eff")
 
 # If we assume the average wave is a sum of plane waves every where, then we can estimate which of these effectives waves will have decayed too much already, and therefore discard them
-decay_effs = abs.(exp.((im*X_match[1]/k).*k_effs))
-eff_inds = find(decay_effs .> 1e-8)
-k_effs = k_effs[eff_inds]
+decay_effs = abs.(exp.((im*X_match[1]/k).*k_effs_all))
+eff_inds = find(decay_effs .> 1e-5)
+k_effs = k_effs_all[eff_inds]
 
 wave_effs = [
     EffectiveWave(ω, k_eff, medium, [specie];
@@ -57,6 +59,7 @@ end
 
 include("match_waves.jl")
 
+invVY = match_only_arrays(ω, wave_effs, XL, X[1:XJ], medium, [specie]; θin = θin)
 (w_vec, q_arr) = extinc_arrays(ω, wave_effs, XL, X[1:XJ], medium, [specie]; θin = θin)
 (L_mat, E_mat, b_eff) = match_arrays(ω, wave_effs, XL, X[1:XJ], medium, [specie]; θin = θin)
 
@@ -66,23 +69,29 @@ qA = sum(q_arr[ind] * avg_wave.amplitudes[ind] for ind in eachindex(avg_wave.amp
 αs = LA + b_eff
 
 # check extinction
-    norm((sum(w_vec .* αs) - (qA + im*k^2))/sum(w_vec .* αs)) < 1e-12
+    norm((sum(w_vec .* αs) - (qA + im*k^2))/sum(w_vec .* αs)) < 1e-10
 
 # check matching
+    αs_match = invVY*avg_wave.amplitudes[:]
     # Calculate the discretised wave from these effective wave
     avg_wave_effs = [AverageWave(real(k), wave, X[XL:Xend]) for wave in wave_effs]
+
     amplitudes_eff = sum(αs[i] * avg_wave_effs[i].amplitudes[:,:,:] for i in eachindex(avg_wave_effs))
     avg_wave_eff = AverageWave(ho, avg_wave_effs[1].X, amplitudes_eff)
+
+    amplitudes_eff = sum(αs_match[i] * avg_wave_effs[i].amplitudes[:,:,:] for i in eachindex(avg_wave_effs))
+    avg_wave_eff_match = AverageWave(ho, avg_wave_effs[1].X, amplitudes_eff)
 
     ps = map(0:ho) do n
         max_w = maximum(real.(avg_wave.amplitudes[XL:end,n+ho+1]))
         min_w = minimum(real.(avg_wave.amplitudes[XL:end,n+ho+1]))
-        max_w = (max_w > 0) ? 1.2*max_w : 0.8*max_w
-        min_w = (min_w < 0) ? 1.2*min_w : 0.8*min_w
+        max_w = (max_w > 0) ? 1.2*max_w : 0.0
+        min_w = (min_w < 0) ? 1.2*min_w : 0.0
         plot(x -> x,y->max_w,X[XL],X[XJ],line=0,fill=(0,:orange), fillalpha=0.4, lab="match region")
         plot!(x -> x,y->min_w,X[XL],X[XJ],line=0,fill=(0,:orange), fillalpha=0.4, lab="")
         plot!(xlab = "X", ylab = "Re amplitude", title="Hankel order $n")
         plot!(avg_wave_eff.X, real.(avg_wave_eff.amplitudes[:,n+ho+1]), lab = "Wave eff")
+        plot!(avg_wave_eff_match.X, real.(avg_wave_eff_match.amplitudes[:,n+ho+1]), lab = "Wave eff match")
         scatter!(X[XL:Xend], real.(avg_wave_X.amplitudes[XL:Xend,n+ho+1]), lab = "Avg wave", linewidth =2 )
         plot!(avg_wave_effs[1].X, real.(avg_wave_effs[1].amplitudes[:,n+ho+1]), lab = "Wave eff 1", linewidth =2, linestyle=:dash)
     end
