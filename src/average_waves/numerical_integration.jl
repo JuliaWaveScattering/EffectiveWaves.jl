4d8fe3935c07ce1756142e5130f49efa9ea7c13f
arturgower <arturgower@gmail.com>
2018-08-29 11:12:03 +0100

    adjuts simpsons scheme to allow for offset x

--- src/average_waves/numerical_integration.jl
+++ src/average_waves/numerical_integration.jl
@@ -37,22 +37,27 @@ function trap_scheme(x::AbstractVector{T}; x0 = first(x), xn = last(x)) where T<
     σs
 end
 
-function simpson_scheme(x::AbstractVector{Float64}; x0=x[1], xn=x[end])
-    if iseven(length(x))
+function simpson_scheme(x::AbstractVector{T}; x0=first(x), xn=last(x))
+    inds = indices(x,1)
+
+    if iseven(length(inds))
         warn("Simpson's integration scheme is designed for an odd number of mesh points")
     end
 
-    h = (x[2]-x[1])
-    σs = [iseven(j) ? 4.0 : 2.0 for j in eachindex(x)]
-    σs = σs*h/3.0
+    h = (x[inds[2]]-x[inds[1]])
+    σs = x.*zero(T)
+    for j in 1:length(inds)
+        σs[inds[j]] = iseven(j) ? 4.0 : 2.0
+    end
+    σs = σs.*(h/3.0)
 
     # accounts for the ends
-    σs[1] = h/3.0
-    σs[1] +=  (x[1] - x0)*(x[2] - x0 + h)/(2.0h)
-    σs[2] +=  - (x0-x[1])^2.0/(2.0h)
-    σs[end] = h/3.0
-    σs[end] +=  (xn-x[end])*(xn - x[end-1] + h)/(2h)
-    σs[end-1] += - (xn-x[end])^2.0/(2h)
+    σs[inds[1]] = h/3.0
+    σs[inds[1]] +=  (x[inds[1]] - x0)*(x[inds[2]] - x0 + h)/(2.0h)
+    σs[inds[2]] +=  - (x0-x[inds[1]])^2.0/(2.0h)
+    σs[inds[end]] = h/3.0
+    σs[inds[end]] +=  (xn-x[inds[end]])*(xn - x[inds[end-1]] + h)/(2h)
+    σs[inds[end-1]] += - (xn-x[inds[end]])^2.0/(2h)
 
     σs
 end
