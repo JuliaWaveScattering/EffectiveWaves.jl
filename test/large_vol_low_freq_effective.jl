5461ea3119c4df3b0820a5bdab5c76aabb557f7e
arturgower <arturgower@gmail.com>
2018-09-03 11:49:48 +0100

    adjusting test to try avoid travis lock

--- test/large_vol_low_freq_effective.jl
+++ test/large_vol_low_freq_effective.jl
@@ -1,16 +1,19 @@
-# large volume fraction scatterers,  small size amd on strong scatterer. This is a problamatic case.
-@testset "large volume fraction and low frequency effective" begin
-    medium = Medium(1.0,1.0+0.0im)
-    species = [
-        Specie(ρ=5.,r=0.004, c=1.2, volfrac=0.4),
-        Specie(ρ=0.3, r=0.002, c=0.4, volfrac=0.3)
-    ]
-    # angular frequencies
-    ωs = [0.001, 2.0, 9.0]
+# angular frequencies
+ωs = [0.001, 2.0, 9.0]
 
-    eff_medium = effective_medium(medium, species)
+medium = Medium(1.0,1.0+0.0im)
+species = [
+    Specie(ρ=5.,r=0.004, c=1.2, volfrac=0.4),
+    Specie(ρ=0.3, r=0.002, c=0.4, volfrac=0.3)
+]
 
-    for ω in ωs
+eff_medium = effective_medium(medium, species)
+
+for ω in ωs
+
+    # large volume fraction scatterers,  small size amd on strong scatterer. This is a problamatic case.
+    @testset "large volume fraction and frequency ak = $(species[2].r*ω)" begin
+
         k_eff_low = ω/eff_medium.c
         # k_effs = wavenumber(ωs, medium, species)
         k_effs = wavenumbers(ω, medium, species; tol = 1e-7)
