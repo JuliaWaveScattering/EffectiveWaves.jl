531d5ac315fee05bd0d79502641e56cf61fe70ff
arturgower <arturgower@gmail.com>
2018-09-20 13:26:39 +0100

    correct and perfect wavenumbes_path

--- src/effective_waves/wavenumber_path.jl
+++ src/effective_waves/wavenumber_path.jl
@@ -4,9 +4,10 @@ function wavenumbers_path(ω::T, medium::Medium{T}, species::Vector{Specie{T}}; 
         num_wavenumbers::Int = 3,
         radius_multiplier::T = T(1.005),
         t_vecs::Vector{Vector{Complex{T}}} = t_vectors(ω, medium, species; hankel_order = hankel_order),
+        verbose::Bool = false,
         kws...) where T<:Number
 
-    low_tol = 1e-4 # a tolerance used for a first pass with time_limit
+    low_tol = max(1e-4, tol) # a tolerance used for a first pass with time_limit
     k = real(ω/medium.c)
     S = length(species)
     ho = hankel_order
@@ -35,8 +36,16 @@ function wavenumbers_path(ω::T, medium::Medium{T}, species::Vector{Specie{T}}; 
         dx = kin[1]*mesh_size
         k_vecs = [[kin[1]+x,kin[2]] for x in linspace(-dx,dx,mesh_points+1)]
         push!(k_vecs,kin, [real(k0),zero(T)], [zero(T),zero(T)])
-        k_vecs = [optimize(detMM2, kvec; x_tol= low_tol).minimizer for kvec in k_vecs]
-        k_vecs = reduce_kvecs(k_vecs, low_tol/20)
+
+        k_vecs = [optimize(detMM2, kvec; x_tol=low_tol).minimizer for kvec in k_vecs]
+        k_vecs = reduce_kvecs(k_vecs, low_tol/10)
+
+        k_vecs = [optimize(detMM2, kvec; g_tol = tol^2.0, f_tol = tol^4.0, x_tol=tol).minimizer for kvec in k_vecs]
+        k_vecs = reduce_kvecs(k_vecs, tol)
+
+        # Delete unphysical waves, including waves travelling backwards with almost no attenuation. This only is important in the limit of very low frequency or very weak scatterers.
+        deleteat!(k_vecs, find(k_vec[2] < -T(10)*tol for k_vec in k_vecs))
+        deleteat!(k_vecs, find(k_vec[2] < tol && k_vec[1] < tol for k_vec in k_vecs))
 
     sort!(k_vecs, by= kv -> kv[2])
     dk_x = abs(k_vecs[1][1]) * mesh_size
@@ -44,56 +53,69 @@ function wavenumbers_path(ω::T, medium::Medium{T}, species::Vector{Specie{T}}; 
     dk_xs = linspace(-dk_x, dk_x, mesh_points)
 
     # Find two more roots, one with larger and smaller imaginary parts than the primary root kin
-        two_roots=false
+        two_roots = false # in case the roots are not on both branches of the tree, which is tested below
         kxs = dk_xs .+ k_vecs[1][1]
         ky =  k_vecs[1][2]
         invert = true
 
+        # Find the first two roots that lead to the two branches of the root tree
         while !two_roots && (length(k_vecs) < num_wavenumbers)
-            ky = ky + dk_y
             hits = [
                 optimize(detMM2, [kx, ky]; x_tol = low_tol).minimizer
             for kx in kxs]
-            deleteat!(hits, find(detMM2.(hits) .> low_tol))
-            k_vecs = reduce_kvecs([hits;k_vecs], low_tol/20)
+            hits = reduce_kvecs(hits, low_tol)
+
+            # Here we refine the hits
+            hits = map(hits) do k_vec
+                res = optimize(detMM2, k_vec; g_tol = tol^2.0, f_tol = tol^4.0, x_tol=tol)
+                if res.minimum > T(20)*tol
+                    [zero(T),-one(T)]
+                else
+                    res.minimizer
+                end
+            end
+            k_vecs = reduce_kvecs([hits;k_vecs], tol)
             sort!(k_vecs, by= kv -> kv[2])
 
-            if length(k_vecs) > 2
-                sort!(k_vecs, by= kv -> kv[2])
+            if length(k_vecs) >= 2
                 min1 = minimum(kv[1] for kv in k_vecs[2:end])
                 max1 = maximum(kv[1] for kv in k_vecs[2:end])
                 if min1*max1 < 0 || (max1-k_vecs[1][1])*(min1-k_vecs[1][1]) < 0
                     two_roots=true
                 end
                 dk_x += dk_x
-                kxs = -k_vecs[2][1] + linspace(-dk_x, dk_x, mesh_points)
+                kxs = -k_vecs[2][1] .+ linspace(-dk_x, dk_x, mesh_points)
+                if invert
+                    dk_y = k_vecs[2][2]/10
+                    ky = k_vecs[2][2] - dk_y
+                    invert = false
+                elseif ky > 4*maximum(ks -> ks[2],k_vecs)
+                    two_roots=true
+                end
             elseif length(k_vecs) == 1
                 dk_y += dk_y
-            elseif invert
-                sort!(k_vecs, by= kv -> kv[2])
-                kxs = dk_xs .- k_vecs[2][1]
-                dk_y = k_vecs[2][2]/10
-                ky = k_vecs[2][2] - dk_y
-                invert = false
-            elseif ky > 4*maximum(ks -> ks[2],k_vecs)
-                two_roots=true
-            else
-                dk_x += dk_x
-                kxs = -k_vecs[2][1] + linspace(-dk_x, dk_x, mesh_points)
             end
+            ky = ky + dk_y
         end
 
     # Find roots following on from the two roots
         sort!(k_vecs, by = kv->kv[2])
         targets = k_vecs[2:end]
-        dys = linspace(0.5*mesh_size,1.0+mesh_size,mesh_points)
+        dys = linspace(0.5,1.0+mesh_size,mesh_points)
         while (length(k_vecs) < num_wavenumbers) && length(targets) > 0
             # find roots following on from the smallest attenuating root
                 sort!(targets, by = kv->kv[2])
+                if verbose println("\n New target: $(targets[1])") end
 
             # find closest neighbour with smaller imaginary part
                 i = findmin((targets[1][2] != kvec[2])? norm(targets[1] - kvec) : Inf for kvec in k_vecs)[2]
-                j = findmin((targets[1][2] != kvec[2] && kvec[2] != k_vecs[i][2])? norm(targets[1] - kvec) : Inf for kvec in k_vecs)[2]
+                # find at least one neighbour below, as the tree is growing
+                j = (targets[1][2] > k_vecs[i][2]) ?
+                    findmin((targets[1][2] != kvec[2] && kvec[2] != k_vecs[i][2])? norm(targets[1] - kvec) : Inf for kvec in k_vecs)[2] :
+                    findmin((targets[1][2] > kvec[2] && kvec[2] != k_vecs[i][2])? norm(targets[1] - kvec) : Inf for kvec in k_vecs)[2]
+                if j == Inf j = i end
+
+                if verbose println("Closest neighbours: $(k_vecs[i]) and $(k_vecs[j])") end
 
             # create a mesh from this
                 meshes = map([i,j]) do ind
@@ -107,36 +129,48 @@ function wavenumbers_path(ω::T, medium::Medium{T}, species::Vector{Specie{T}}; 
                 mesh = vcat(meshes...)
                 deleteat!(mesh, find(vec[2] < 0 for vec in mesh))
 
+# scatter([m[1] for m in mesh], [m[2] for m in mesh])
+# scatter!([targets[1][1]], [targets[1][2]])
+# scatter!([m[1] for m in k_vecs[[i,j]]], [m[2] for m in k_vecs[[i,j]]])
+# scatter!([m[1] for m in k_vecs], [m[2] for m in k_vecs])
+
             # search for roots from this mesh
-                hits = map(mesh) do kin
+                new_targets = map(mesh) do kin
                    optimize(detMM2, kin; x_tol = low_tol, f_tol = low_tol/10).minimizer
                 end
-                hits = reduce_kvecs(hits, low_tol/20)
-                deleteat!(hits, find(detMM2.(hits) .> low_tol))
+                new_targets = reduce_kvecs(new_targets, low_tol/10)
+                deleteat!(new_targets, find(detMM2.(new_targets) .> low_tol))
+                # sort!(new_targets, by = kv->kv[2])
 
-            # find new targets
+            # reduce new targets
                 new_targets = [
-                        (findmin([norm(h - kvec) for kvec in k_vecs])[1] > low_tol)? h : [zero(T),-one(T)]
-                    for h in hits]
+                        (findmin([norm(h - kvec) for kvec in k_vecs])[1] > low_tol/10)? h : [zero(T),-one(T)]
+                for h in new_targets]
+                new_targets = reduce_kvecs(new_targets, low_tol)
 
+                # Here we refine the new roots
+                new_targets = map(new_targets) do k_vec
+                    res = optimize(detMM2, k_vec; g_tol = tol^2.0, f_tol = tol^4.0, x_tol=tol)
+                    if res.minimum > T(20)*tol
+                        [zero(T),-one(T)]
+                    else
+                        res.minimizer
+                    end
+                end
+
+                new_targets = [
+                        (findmin([norm(h - kvec) for kvec in k_vecs])[1] > 5*tol)? h : [zero(T),-one(T)]
+                for h in new_targets]
+                new_targets = reduce_kvecs(new_targets, tol)
+
+                if verbose println("New roots: $(new_targets)") end
                 deleteat!(targets, 1)
                 targets = [targets; new_targets]
-                deleteat!(targets, find(vec[2] < 0 for vec in targets))
-                k_vecs = reduce_kvecs([hits;k_vecs], low_tol/20)
+                # group together wavenumbers which are closer than tol
+                k_vecs = reduce_kvecs([new_targets; k_vecs], tol)
+                # scatter([m[1] for m in k_vecs], [m[2] for m in k_vecs])
+                # scatter!([m[1] for m in new_targets], [m[2] for m in new_targets])
         end
-
-    # Here we refine the effective wavenumbers
-    k_vecs = map(k_vecs) do k_vec    # Here we refine the effective wavenumbers
-        res = optimize(detMM2, k_vec; g_tol = tol^2.0, f_tol = tol^4.0, x_tol=tol)
-        if res.minimum > T(20)*tol
-            [zero(T),-one(T)]
-        else
-            res.minimizer
-        end
-    end
-
-    # group together wavenumbers which are closer than tol
-    k_vecs = reduce_kvecs(k_vecs,tol)
 
     # Finally delete unphysical waves, including waves travelling backwards with almost no attenuation. This only is important in the limit of very low frequency or very weak scatterers.
     deleteat!(k_vecs, find(k_vec[2] < -T(10)*tol for k_vec in k_vecs))
--- test/match_average_wave.jl
+++ test/match_average_wave.jl
@@ -17,18 +17,23 @@
         effective_waves(ω, medium, [s];
             hankel_order=hankel_order,
             mesh_points=6,
-            num_wavenumbers=10,
+            num_wavenumbers=28,
             tol = tol,  θin = θin,
             extinction_rescale = false)
     for s in species];
+
+   # use only 10 least attenuating
+   wave_effs_arr = [w[1:10] for w in wave_effs_arr]
 
     match_ws = [
         MatchWave(ω, medium, species[i];
             hankel_order=hankel_order,
             θin = θin, tol = tol,
             wave_effs = wave_effs_arr[i],
-            max_size=80)
-    for i in eachindex(species)]
+            max_size=60)
+    for i in eachindex(species)];
+
+    @test maximum(match_error.(match_ws)) < 1e-4
 
     avgs = [
         AverageWave(ω, medium, species[i];
@@ -37,12 +42,15 @@
                 wave_effs = wave_effs_arr[i], max_size=500)
     for i in eachindex(species)]
 
+    # R_ms = [reflection_coefficient(ω, match_ws[i].average_wave, medium, species[i]) for i in eachindex(species)]
+    # R_ds = [reflection_coefficient(ω, avgs[i], medium, species[i]) for i in eachindex(species)]
+
     map(eachindex(species)) do i
         j0 = findmin(abs.(avgs[i].x .- match_ws[i].x_match[1]))[2]
         x0 = avgs[i].x[j0+1:end]
         avg_m = AverageWave(x0, match_ws[i].effective_waves)
         maximum(abs.(avgs[i].amplitudes[j0+1:end,:,:][:] - avg_m.amplitudes[:]))
         @test norm(avgs[i].amplitudes[j0+1:end,:,:][:] - avg_m.amplitudes[:])/norm(avg_m.amplitudes[:]) < 4e-3
-        @test maximum(abs.(avgs[i].amplitudes[j0+1:end,:,:][:] - avg_m.amplitudes[:])) < 4e-3
+        @test maximum(abs.(avgs[i].amplitudes[j0+1:end,:,:][:] - avg_m.amplitudes[:])) < 1e-3
     end
 end
