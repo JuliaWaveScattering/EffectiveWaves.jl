92203fd6507c7944b69714dd6653620a5d9089eb
ArtGower <arturgower@gmail.com>
2018-09-25 14:00:52 +0100

    adjust integrate refleciton to make clear correction

--- src/average_waves/reflection.jl
+++ src/average_waves/reflection.jl
@@ -3,9 +3,9 @@ function reflection_coefficient(ω::T, amps::AverageWave{T}, medium::Medium, spe
 
     k = ω/medium.c
     M = amps.hankel_order
-    σ = trap_scheme(amps.x)
+    σ = k*trap_scheme(amps.x) # multiple by k to be the same as the non-dimensional version
 
-    R = T(2)*specie.num_density/(cos(θin)*k)*sum(
+    R = T(2)*specie.num_density/(cos(θin)*k^2)*sum(
         im^T(m)*exp(im*k*amps.x[j]*cos(θin) - im*θin*m)*amps.amplitudes[j,m+M+1,1]*σ[j]
     for m=-M:M, j in eachindex(amps.x))
 
