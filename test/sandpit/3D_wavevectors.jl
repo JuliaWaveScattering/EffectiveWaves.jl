0c2f66d79aacb6a9a4d580e503c7501a0ebe6bc3
arturgower <arturgower@gmail.com>
2019-08-13 17:35:02 +0100

    saved spherical wavenumbers

--- test/sandpit/3D_wavevectors.jl
+++ test/sandpit/3D_wavevectors.jl
@@ -18,9 +18,9 @@ kps = wavenumbers(ω, medium, species; symmetry = :plane, dim = 3,
     mesh_points = 14, mesh_size = 2.0)
 
 # Doesnt really work
-R_kps = wavenumbers(ω, medium, species; dim = 3, hankel_order = hankel_order,
-    num_wavenumbers=27, tol = 1e-8, mesh_points = 10, mesh_size = 2.0)
-
+# R_kps = wavenumbers(ω, medium, species; dim = 3, hankel_order = hankel_order,
+#     num_wavenumbers=27, tol = 1e-8, mesh_points = 10, mesh_size = 2.0)
+R_kps = Complex{Float64}[2.30521+1.85168im, -1.46227+2.03392im, -1.45281+2.03493im, 10.9431+5.60625im, -10.0589+5.61005im, -10.0562+5.61218im, 19.1007+6.88548im, -18.2139+6.88838im, 27.0945+7.7147im, 35.0209+8.32989im, 35.0222+8.33306im, 42.9157+8.82717im, 50.7889+9.23896im, 58.6476+9.58905im, 58.649+9.59209im, 66.5001+9.90123im, 74.3447+10.1762im, 82.1845+10.4237im, 90.0205+10.6489im]
 
 wavesystem2D = wavematrix2D(ωs[1], medium, species; hankel_order=hankel_order);
 det2D(kp) = det(wavesystem2D(kp))
