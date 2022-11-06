2c185a940d1ecc44353f97984a83e740c4b24ade
arturgower <arturgower@gmail.com>
2022-11-06 19:38:00 +0000

    gs typo

--- src/acoustics/effective_wave/special_functions.jl
+++ src/acoustics/effective_wave/special_functions.jl
@@ -14,8 +14,8 @@ end
 
 function kernelW3D(k::Union{T,Complex{T}}, keff::Complex{T}, ps::AbstractMatrix{DP}, basis_order::Int) where {T<:AbstractFloat, DP <: DiscretePairCorrelation}
 
-    S = size(gs,1)
-    r = gs[1].r
+    S = size(ps,1)
+    r = ps[1].r
 
     σ = integration_scheme(r)
 
