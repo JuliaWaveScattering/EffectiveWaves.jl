132ab2ee555dac9d5ef901c25637262e6b56b2aa
pivaps <pspiva1@sheffield.ac.uk>
2022-12-05 19:16:23 +0000

    Adding new eigensystem for translation symmetry

--- src/acoustics/effective_wave/regular/eigensystem.jl
+++ src/acoustics/effective_wave/regular/eigensystem.jl
@@ -160,3 +160,84 @@ function eigensystem(ω::T, micro::ParticulateMicrostructure{3}, ::RadialSymmetr
 
         return MMR
 end
+
+# The eigensystem when translation symmetry is present
+function eigensystem(ω::T, micro::ParticulateMicrostructure{3}, ::AbstractTranslationSymmetry{3};
+        basis_order::Int = 2,
+        basis_field_order::Int = 2*basis_order,
+        # numberofparticles::Number = Inf,
+        kws...) where T<:AbstractFloat
+
+    medium = micro.medium
+
+    k = ω/medium.c
+    sps = micro.species
+
+    S = length(sps)
+    L = basis_order
+    L1 = basis_field_order
+    len = (2L1+1) * (L+1)^2 * S
+    MM_mat = Matrix{Complex{T}}(undef,len,len)
+
+    t_matrices = get_t_matrices(medium, sps, ω, L)
+    t_diags = diag.(t_matrices)
+    len(order::Int) = basisorder_to_basislength(Acoustic{T,3},order)
+
+    as = [
+        micro.paircorrelations[i,j].minimal_distance
+    for i in eachindex(sps), j in eachindex(sps)]
+
+    if length(micro.paircorrelations[1].r) > 1
+        pair_rs, hks, gs = precalculate_pair_correlations(micro, k, ho)
+    end
+
+    function M_component(keff,Ns,l,m,dl,dm,m1,m2,s2)::Complex{T}
+        minl1 = abs(l - dl)
+        maxl1 = l + dl
+
+        (m == dm && l == dl && m1 == m2 && s1 == s2 ? 1.0 : 0.0) +
+        if minl1 <= maxl1
+            number_density(sps[s2]) * t_diags[s1][len(l)] *
+            sum(l1 ->
+                if abs(m2 - m1) <= l1
+                    1im^(l1 + m1 - m2) *
+                    gaunt_coefficient(dl,dm,l,m,l1,m1-m2) *
+                    spherical_harmonics(l1, pi/2, 0.0)[(l1^2 + l1 + 1) + (m2 - m1)] *
+                    Ns[l1+1,s1,s2]
+                else
+                    zero(Complex{T})
+                end
+            , minl1:maxl1)
+        else
+            zero(Complex{T})
+        end
+    end
+
+    function MM(keff::Complex{T})::Matrix{Complex{T}}
+
+        Ns = [
+            as[s1,s2] * kernelN3D(l1,k*as[s1,s2],keff*as[s1,s2])
+        for l1 = 0:2L, s1 = 1:S, s2 = 1:S]
+
+        # For a pair correlation which is not hole correction need to add a finite integral
+        if length(micro.paircorrelations[1].r) > 1
+            Ns = Ns - kernelW3D(k, keff, pair_rs, gs, hks, basis_order)
+        end
+
+        Ns = Ns ./  (keff^2.0 - k^2.0)
+
+        # The order of the indices below is important
+        ind2 = 1
+        for s2 = 1:S for dl = 0:L for dm = -dl:dl for m1 = -L1:L1
+            ind1 = 1
+            for s1 = 1:S for l = 0:L for m = -l:l for m2 = -L1:L1
+                MM_mat[ind1, ind2] = M_component(keff,Ns,l,m,dl,dm,m1,m2,s2)
+                ind1 += 1
+            end end end end
+            ind2 += 1
+        end end end end
+        return MM_mat
+    end
+
+    return MM
+end
