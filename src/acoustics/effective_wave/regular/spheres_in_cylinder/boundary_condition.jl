8c0a47eefb07b3ba9638920e0a7fcb181d785d7d
pivaps <pspiva1@sheffield.ac.uk>
2023-02-03 11:46:18 +0000

    Final version of boundary conditions spheres in cylinder

--- src/acoustics/effective_wave/regular/spheres_in_cylinder/boundary_condition.jl
+++ src/acoustics/effective_wave/regular/spheres_in_cylinder/boundary_condition.jl
@@ -1,6 +1,6 @@
 function solve_boundary_condition(ω::T, k_eff::Complex{T}, eigvectors::Array{Complex{T}}, source::AbstractSource{Acoustic{T,2}}, material::Material{Circle{T,2}}, ::TranslationSymmetry{3,T};
         basis_order::Int = 2,
-        basis_field_order::Int = 2*basis_order,
+        basis_field_order = 2 * basis_order
         source_basis_field_order::Int = Int((size(eigvectors)[3] - 1) / 2),
         kws...
     ) where T
@@ -26,18 +26,23 @@ function solve_boundary_condition(ω::T, k_eff::Complex{T}, eigvectors::Array{Co
     L = basis_order
     M = basis_field_order
     Minc = source_basis_field_order
+    M1 = L + Minc
+    if M1 > M
+        M1 = M
+        @warn "Adjusting field order expansions for the system to be solvable"
+    end
 
     # the kernel used to weight the species and the field's basis order.
     Ns = [
         kernelN2D(m, k0*(R - rs[j]), k_eff*(R - rs[j])) * number_density(species[j])
-    for m = -M:M, j in eachindex(species)] ./ (k_eff^T(2) - k0^T(2))
+    for m = -M1:M1, j in eachindex(species)] ./ (k_eff^T(2) - k0^T(2))
 
-    dn_m = [[dl,dm,m] for dl = 0:L for dm = -dl:dl for m = -M:M];
+    dn_m = [[dl,dm,m] for dl = 0:L for dm = -dl:dl for m = -M1:M1];
     lm_to_n = lm_to_spherical_harmonic_index
 
     vecs = [
-        eigvectors[(lm_to_n(i[1],i[2]) - 1) * (2M + 1) + i[3] + M + 1, j, p] *
-        Ns[i[3] + M + 1]
+        eigvectors[(lm_to_n(i[1],i[2]) - 1) * (2M1 + 1) + i[3] + M1 + 1, j, p] *
+        Ns[i[3] + M1 + 1]
     for i in dn_m, j in eachindex(species), p = 1:(2Minc + 1)];
 
     # sum over species
@@ -65,9 +70,9 @@ function solve_boundary_condition(ω::T, k_eff::Complex{T}, eigvectors::Array{Co
         sum(l ->
             sum(m ->
                 Complex{T}(1im)^(l + m) * Ys[lm_to_n(l, m)] .*
-                vecs[(lm_to_n(l,m)-1) * (2M+1) + (m-s) + M + 1, p]
+                vecs[(lm_to_n(l,m)-1) * (2M1+1) + (m-s) + M1 + 1, p]
             ,-l:l)
-        , 0:L) for s = -Minc:Minc, p = 1:(2Minc + 1)];
+        , 0:(M1-Minc)) for s = -Minc:Minc, p = 1:(2Minc + 1)];
 
     n_n1 = [[l,m,l1,m1] for l = 0:L for m = -l:l for l1 = 0:L for m1 = -l1:l1];
     wall_contribution = [
@@ -88,7 +93,7 @@ function solve_boundary_condition(ω::T, k_eff::Complex{T}, eigvectors::Array{Co
                 sum(dl ->
                     sum(dm ->
                         gaunt_coefficient(dl,dm,i[1],i[2],l2,m2) *
-                        vecs[(lm_to_n(dl,dm)-1) * (2M+1) + (i[4]-m2) + M + 1, p]
+                        vecs[(lm_to_n(dl,dm)-1) * (2M+1) + (i[4]-m2) + M1 + 1, p]
                     , -dl:dl)
                 , 0:L)
             , -l2:l2)
@@ -97,6 +102,7 @@ function solve_boundary_condition(ω::T, k_eff::Complex{T}, eigvectors::Array{Co
     # Incident wave coefficients
     source_coefficients = Tran .* regular_spherical_coefficients(source)(Minc,zeros(2),ω)
 
+    # Forcing term
     forcing = [Complex{T}(-1) *
         sum(dl ->
             sum(dm ->
@@ -105,14 +111,46 @@ function solve_boundary_condition(ω::T, k_eff::Complex{T}, eigvectors::Array{Co
                 source_coefficients[dm + Minc + 1]
             , -dl:dl)
         , 0:L)
-        for i in indices]
+        for i in n_n1]
 
+    # Full matrix to be inverted
     matrix = (2pi^2/k0) * (particle_contribution + wall_contribution)
 
-    α = matrix \ forcing
+    # Reduction of matrix and forcing term
+    square_matrix = [
+        sum(l ->
+            sum(m ->
+                sum(dl ->
+                    sum(dm ->
+                        matrix[(lm_to_n(l,m)-1)*(L+1)^2 + lm_to_n(dl,dm), p] *
+                        sum(ddl ->
+                            gaunt_coefficient(ddl,ddm,l,m,dl,dm)
+                        , abs(ddm):Minc)
+                    , -dl:dl)
+                , 0:L)
+            , -l:l)
+        , 0:L) for ddm in -Minc:Minc, p in 1:(2Minc + 1)]
 
-    err = norm(forcing - matrix * α) / norm(forcing)
+    reduced_forcing = [
+        sum(l ->
+            sum(m ->
+                sum(dl ->
+                    sum(dm ->
+                        forcing[(lm_to_n(l,m)-1)*(L+1)^2 + lm_to_n(dl,dm)] *
+                        sum(ddl ->
+                            gaunt_coefficient(ddl,ddm,l,m,dl,dm)
+                        , abs(ddm):Minc)
+                    , -dl:dl)
+                , 0:L)
+            , -l:l)
+        , 0:L) for ddm in -Minc:Minc]
+    
+    # Computing normalization factors
+    α = square_matrix \ reduced_forcing
 
+    # Checking error in the solution of the system
+    err = norm(reduced_forcing - square_matrix * α) / norm(reduced_forcing)
+    
     if err > sqrt(eps(T))
         @warn "Extinction equation (like a boundary condition) was solved with an error: $err for the effective wavenumber: $k_eff"
     end
