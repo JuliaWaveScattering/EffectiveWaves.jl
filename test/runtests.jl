bcdba2c6b3c2b56eeaa7f678cb011d77138ed34d
arturgower <arturgower@gmail.com>
2026-02-17 21:13:20 +0000

    add test for bisection robust

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using ClassicalOrthogonalPolynomials
 end
 
 include("complex.jl")
-# include("wavenumbers-solvers.jl")
+include("wavenumbers-solvers.jl")
 
 # Single effective wavenumber tests
     include("acoustics-2D/strong_low_freq_effective.jl")
